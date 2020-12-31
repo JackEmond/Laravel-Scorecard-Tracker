9240bb824eb17c6d340e7a477e2391bb36e728bf
Jack Emond <JackEmond@outlook.com>
2020-12-31 01:02:48 -0500

    Update design of which course did you play page

--- resources/views/scorecards/numberofholesplayed.blade.php
+++ resources/views/scorecards/numberofholesplayed.blade.php
@@ -3,23 +3,20 @@
 @section('content')
 
 <h2>How many holes did you play?</h2>
+
 <div class="holes-width">
-    <div class="holes-box">
-    <br>
-        <h1>Nine Holes</h1>
-        <a href="/scorecards/Select_a_Course/9">
-        <img class="holes-img" src="{{URL('/images/golfbag.png')}}"/>
-        </a>
-    </div>
-    <div class="holes-box right">
-        <br>
-        <h1>Eighteen Holes</h1>
-        <a href="/scorecards/Select_a_Course/18">
+    <a href="/scorecards/Select_a_Course/9">
+        <div class="holes-box">
+            <h1>Nine Holes</h1>
+            <img class="holes-img" src="{{URL('/images/golfbag.png')}}"/>
+        </div>
+    </a>
+    <a href="/scorecards/Select_a_Course/18">
+        <div class="holes-box right">
+            <h1>Eighteen Holes</h1>
             <img class="holes-img" src="{{URL('/images/golfcart.png')}}"/>
-        </a>
-
-    </div>
+        </div>
+    </a>
 </div>    
-
 
 @endsection
